99b862ccd48a7b3d0263f4a8d117d12c2cf7320f
Claudia Carvalho <claudiacarsanttos@gmail.com>
2021-10-27 18:05:12 -0300

    Primeiro jogo mobile utilizando a engine Unity.

---
+++ Library/Il2cppBuildCache/Android/armeabi-v7a/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,95 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CriarPoder::Start()
+extern void CriarPoder_Start_mBFEDCF09F60683E8D3A2AF12E73D2902E7946F72 (void);
+// 0x00000002 System.Void CriarPoder::Update()
+extern void CriarPoder_Update_m5E3A76273DBC01C5D647422DC84BD170C4AB8DA3 (void);
+// 0x00000003 System.Void CriarPoder::.ctor()
+extern void CriarPoder__ctor_m3723E7552DBBA5A3EB709D8EB3F39F30F0227EAD (void);
+// 0x00000004 System.Void MovimentarPersonagem::Start()
+extern void MovimentarPersonagem_Start_mB41A4760215A0CBF6F77CF3F7DA1E84AF8A9142E (void);
+// 0x00000005 System.Void MovimentarPersonagem::Update()
+extern void MovimentarPersonagem_Update_m69E3B737266ADACB03BC479485B801C351B29CB1 (void);
+// 0x00000006 System.Void MovimentarPersonagem::.ctor()
+extern void MovimentarPersonagem__ctor_mA15135C184C6C6F970ED62984F5787755EC400EF (void);
+// 0x00000007 System.Void MovimentarPoder::Start()
+extern void MovimentarPoder_Start_mF89D005363CB9B7290C3F23F83703ECD4898802B (void);
+// 0x00000008 System.Void MovimentarPoder::Update()
+extern void MovimentarPoder_Update_m3B2C3CFD7AE5F5E0A746DBF694B454E55DD649BB (void);
+// 0x00000009 System.Void MovimentarPoder::.ctor()
+extern void MovimentarPoder__ctor_mE55A97DFF475A3EDBB47987F4749C6D63656044F (void);
+// 0x0000000A System.Void PegaBola::Start()
+extern void PegaBola_Start_m7EED301550D106F1F97C8CD4E278AF1CFC546757 (void);
+// 0x0000000B System.Void PegaBola::Update()
+extern void PegaBola_Update_m5A62D3E372C8A1DD6CFB6B25119A3C6E6E3C9891 (void);
+// 0x0000000C System.Void PegaBola::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern void PegaBola_OnTriggerEnter2D_m9C9F8051095A13EB28F07532EC493F9F70845D77 (void);
+// 0x0000000D System.Void PegaBola::.ctor()
+extern void PegaBola__ctor_mA02D38542F99619EB1A9B9F9F3DB2C79C45B5294 (void);
+static Il2CppMethodPointer s_methodPointers[13] = 
+{
+	CriarPoder_Start_mBFEDCF09F60683E8D3A2AF12E73D2902E7946F72,
+	CriarPoder_Update_m5E3A76273DBC01C5D647422DC84BD170C4AB8DA3,
+	CriarPoder__ctor_m3723E7552DBBA5A3EB709D8EB3F39F30F0227EAD,
+	MovimentarPersonagem_Start_mB41A4760215A0CBF6F77CF3F7DA1E84AF8A9142E,
+	MovimentarPersonagem_Update_m69E3B737266ADACB03BC479485B801C351B29CB1,
+	MovimentarPersonagem__ctor_mA15135C184C6C6F970ED62984F5787755EC400EF,
+	MovimentarPoder_Start_mF89D005363CB9B7290C3F23F83703ECD4898802B,
+	MovimentarPoder_Update_m3B2C3CFD7AE5F5E0A746DBF694B454E55DD649BB,
+	MovimentarPoder__ctor_mE55A97DFF475A3EDBB47987F4749C6D63656044F,
+	PegaBola_Start_m7EED301550D106F1F97C8CD4E278AF1CFC546757,
+	PegaBola_Update_m5A62D3E372C8A1DD6CFB6B25119A3C6E6E3C9891,
+	PegaBola_OnTriggerEnter2D_m9C9F8051095A13EB28F07532EC493F9F70845D77,
+	PegaBola__ctor_mA02D38542F99619EB1A9B9F9F3DB2C79C45B5294,
+};
+static const int32_t s_InvokerIndices[13] = 
+{
+	864,
+	864,
+	864,
+	864,
+	864,
+	864,
+	864,
+	864,
+	864,
+	864,
+	864,
+	747,
+	864,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	13,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
